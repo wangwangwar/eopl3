b7fdb97d6d875ae76a4289881690b05a4e5f58c8
wangwangwar <wangwangwar@gmail.com>
2017-04-20 11:09:57 +0800

    Add Log

--- chapter7/inferred/inferrer.scm
+++ chapter7/inferred/inferrer.scm
@@ -29,6 +29,7 @@
         (a-program (exp1)
           (cases answer (type-of exp1 (init-tenv) (empty-subst))
             (an-answer (ty subst)
+              (eopl:printf "*type-of-program*\n  exp1: ~s\n  type: ~s\n  subst: ~s\n\n" exp1 ty subst)
               (apply-subst-to-type ty subst)))))))
 
   ;; type-of : Exp * Tenv * Subst -> Type
@@ -37,17 +38,21 @@
     (lambda (exp tenv subst)
       (cases expression exp
         
-        (const-exp (num) (an-answer (int-type) subst))
+        (const-exp (num) 
+          (eopl:printf "*type-of const-exp*\n  exp1: ~s\n  type1: int-type\n  subst1: ~s\n\n" num subst)
+          (an-answer (int-type) subst))
       
         (zero?-exp (exp1)
           (cases answer (type-of exp1 tenv subst)
             (an-answer (type1 subst1)
+              (eopl:printf "*type-of zero?-exp*\n  exp1: ~s\n  type1: ~s\n  subst1: ~s\n\n" exp1 type1 subst1)
               (let ((subst2 (unifier type1 (int-type) subst1 exp)))
                 (an-answer (bool-type) subst2)))))
 
         (diff-exp (exp1 exp2)
           (cases answer (type-of exp1 tenv subst)
             (an-answer (type1 subst1)
+              (eopl:printf "*type-of diff-exp*\n  exp1: ~s\n  type1: ~s\n  subst1: ~s\n\n" exp1 type1 subst1)
               (let ((subst1 (unifier type1 (int-type) subst1 exp1)))
                 (cases answer (type-of exp2 tenv subst1)
                   (an-answer (type2 subst2)
@@ -64,12 +69,17 @@
                   (an-answer (ty2 subst)
                     (cases answer (type-of exp3 tenv subst)
                       (an-answer (ty3 subst)
+                        (eopl:printf "*type-of if-exp*\n  exp3: ~s\n  type3: ~s\n  subst: ~s\n\n" exp3 ty3 subst)
                         (let ((subst (unifier ty2 ty3 subst exp)))
                           (an-answer ty2 subst))))))))))
 
-        (var-exp (var) (an-answer (apply-tenv tenv var) subst))
+        (var-exp (var) 
+          (let ((var-type (apply-tenv tenv var)))
+            (eopl:printf "*type-of var-exp*\n  var: ~s\n  type: ~s\n  subst: ~s\n\n" var var-type subst)
+            (an-answer var-type subst)))
 
         (let-exp (var exp1 body)
+          (eopl:printf "*type-of let-exp*\n  var: ~s\n  exp1: ~s\n  body: ~s\n\n" var exp1 body)
           (cases answer (type-of exp1 tenv subst)
             (an-answer (rhs-type subst)
               (type-of body
@@ -78,6 +88,7 @@
 
         (proc-exp (var otype body)
           (let ((arg-type (otype->type otype)))
+            (eopl:printf "*type-of proc-exp*\n  var: ~s\n  otype: ~s\n  body: ~s\n\n" var otype body)
             (cases answer (type-of body
                             (extend-tenv var arg-type tenv)
                             subst)
@@ -92,6 +103,8 @@
               (an-answer (rator-type subst)
                 (cases answer (type-of rand tenv subst)
                   (an-answer (rand-type subst)
+                    (eopl:printf "*type-of call-exp*\n  rator-type: ~s\n  rand-type: ~s\n  subst: ~s\n\n" 
+                                 rator-type rand-type subst)
                     (let ((subst
                             (unifier rator-type
                               (proc-type rand-type result-type)
@@ -117,6 +130,7 @@
                                 bvar proc-arg-type tenv-for-letrec-body)
                               subst)
                 (an-answer (proc-body-type subst)
+                  (eopl:printf "*type-of letrec-exp*\n  proc-body-type: ~s\n  subst: ~s\n\n" proc-body-type subst)
                   (let ((subst 
                           (unifier proc-body-type proc-result-type subst
                             proc-body))) 
